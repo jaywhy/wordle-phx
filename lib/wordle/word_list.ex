7329fc362b743fc9d5ed4d92efe4ae4ec089c343
Jason Yates <jason@jasonyates.me>
2022-07-20 12:19:37 -0400

    Fixed a couple bugs with bad_word? function.

--- lib/wordle/word_list.ex
+++ lib/wordle/word_list.ex
@@ -14,15 +14,13 @@ defmodule Wordle.WordList do
     allowed_list_path()
     |> File.stream!()
     # Could be faster with a binary search. The list is ordered.
-    |> Enum.any?(&(&1 == word))
-    |> Kernel.!()
+    |> Enum.any?(&(&1 |> String.trim() == word))
   end
 
   defp word_list?(word) do
     word_list_path()
     |> File.stream!()
-    |> Enum.any?(&(&1 == word))
-    |> Kernel.!()
+    |> Enum.any?(&(&1 |> String.trim() == word))
   end
 
   defp word_list_path do
